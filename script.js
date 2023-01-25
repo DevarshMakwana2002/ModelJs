e03ac7bfe0b090b92b72eaaf2812a212888a8a0e
DevarshMakwana2002 <devarshppsv@gmail.com>
2023-01-25 18:17:18 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,23 @@
+'use strict'
+
+const model = document.querySelector(".modal")
+
+const overlay = document.querySelector(".overlay")
+
+const btnCloseModel = document.querySelector(".close-modal")
+
+const btnOpenModel = document.querySelectorAll(".show-modal")
+for(let i of btnOpenModel){
+
+    i.addEventListener('click',()=>{
+        console.log("Button clicked !")
+        model.classList.remove('hidden')
+        overlay.classList.remove('hidden')
+    })
+}
+
+btnCloseModel.addEventListener('click',()=>{
+    model.classList.add('hidden')
+    overlay.classList.add('hidden')
+})
+
